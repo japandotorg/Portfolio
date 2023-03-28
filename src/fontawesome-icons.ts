25a7cfbdb318f5a3f306363472ca1a082f542d6e
Lemon Rose <yash.kul69@gmail.com>
2023-03-28 23:22:52 +0530

    fontawesome icons

---
+++ src/fontawesome-icons.ts
@@ -0,0 +1,7 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faArrowRightLong);
+
+export default FontAwesomeIcon;
