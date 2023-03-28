136a11c093be3bdf160ef417b0931a53b23b231f
Lemon Rose <yash.kul69@gmail.com>
2023-03-28 23:23:35 +0530

    app

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <router-view />
+    </div>
+</template>
+
+<style>
+    @font-face {
+        font-family: "Comic Sans MS";
+        src: url("./fonts/comic-sans.ttf");
+    }
+
+    #app {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 101.5%;
+        padding: 2px;
+    }
+ 
+    * {
+        font-family: "Comic Sans MS";
+        margin: 0;
+        padding: 0;
+        border: 0;
+    }
+
+    html,
+    body {
+        height: 99%;
+        width: 99%;
+    }
+</style>
