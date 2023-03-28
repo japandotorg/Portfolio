b3c5fdee5d3158c7a14fa69088f9574d65933d0f
Lemon Rose <yash.kul69@gmail.com>
2023-03-28 23:23:30 +0530

    home page

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <Terminal />
+</template>
+
+<script lang="ts">
+    import { Options, Vue } from "vue-class-component";
+    import Terminal from "../components/Terminal.vue";
+
+    @Options(
+        {
+            components: {
+                Terminal,
+            },
+        }
+    )
+
+    export default class Home extends Vue {}; 
+</script>
